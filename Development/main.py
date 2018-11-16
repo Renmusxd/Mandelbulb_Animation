dd254d364cfab552f7f040c88ea150bbf21ee92d
Joey <joey@169-231-119-129.wireless.ucsb.edu>
2018-11-16 15:53:17 -0800

    updated

--- Development/main.py
+++ Development/main.py
@@ -1,24 +1,24 @@
-from mandelset import space
+from mandel import space
 import numpy as np
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import time 
 
-def display(Vmax=10):
-	t0=time.perf_counter()
-	x,y,z = space(Vmax)
-	t=time.perf_counter()-t0
-	print('Time elapsed During space generation:', t ,'sec')
-
+def display(Vmax=10,n=8,itr=32):
+	x,y,z = space(Vmax=Vmax, n=n, itr=itr)
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(x,y,z, marker='o')
+	ax.scatter(x,y,z, c='r', marker='o')
 	ax.set_xlabel('X Label')
 	ax.set_ylabel('Y Label')
 	ax.set_zlabel('Z Label')
 
 	fig.show()
+	t=time.perf_counter()
 	input('Press <Enter> to exit')
+	return(t)
 
 if __name__=='__main__':
-	display()
+	t0 = time.perf_counter()
+	t=display(Vmax=200,n=8)-t0 # keep Vmax^3 reasonable!
+	print('Time elapsed during operation:', t ,'sec')
--- Development/mandel.py
+++ Development/mandel.py
@@ -1,43 +1,47 @@
 import numpy as np
 from numba import jit
-import time
+from tqdm import tqdm
 
-@jit 
-def member(Cx, Cy, Cz,n = 8,itr = 32):
-    '''Determine if a point is in the mandelbulb set.'''
+@jit
+def member(Cx, Cy, Cz, n, itr):
+    '''Determines if a point with 3 coordinates is in the mandelbulb set.'''
     # start 'orbit' from <0,0,0>
     x, y, z = 0.0, 0.0, 0.0
     
     for i in range(itr): #iteration threshold
         # White and Nylander's formula for the nth power (wikipedia)
-        r = np.sqrt(x*x + y*y + z*z)
+
+        # first move to spherical coordinates to apply recursion formula
+        r = np.sqrt(x*x + y*y + z*z) 
         theta = np.arctan2(np.sqrt(x*x + y*y), z)
         phi = np.arctan2(y, x)
         
+        # apply recursion formula
         x = r**n * np.sin(theta*n) * np.cos(phi*n) + Cx
         y = r**n * np.sin(theta*n) * np.sin(phi*n) + Cy
         z = r**n * np.cos(theta*n) + Cz
         
-        if (x**2 + y**2 + z**2 > 2).any(): # vector length threshold
-            return False
+        if x**2 + y**2 + z**2 > 2: # vector length threshold
+            return False # Return boolean logic. If surpass bound after itr, not set number
     else:
-        return True
+        return True # If remains bounded through number itr, then set member.
 
 @jit
-def space(Vmax=10):
+def space(Vmax = 10, n=8, itr=32):
     '''Generates a set of points in 3D space that are members of the mandelbulb set.'''
-    x,y,z=np.mgrid[-Vmax:Vmax,-Vmax:Vmax,-Vmax:Vmax]
-    x=x.reshape(1,x.size)[0] #flatten out values so as easier to parse, and get rid of nested arrays
-    y=y.reshape(1,y.size)[0]
-    z=z.reshape(1,z.size)[0]
-    spacex=np.empty((1,x.size))
-    spacey=np.empty((1,y.size))
-    spacez=np.empty((1,z.size))
-    for i in range(x.size):
-        if member(x[i],y[i],z[i]):
-           spacex=x[i]
-           spacey=y[i]
-           spacez=z[i]       
+    print(' :: Generating Meshgrid :: ')
+    x,y,z = np.mgrid[-Vmax:Vmax,-Vmax:Vmax,-Vmax:Vmax]
+    print(' :: Generating Axes :: ')
+    x = x.reshape(1,x.size)[0] #flatten out values so as easier to parse, and get rid of nested arrays
+    y = y.reshape(1,y.size)[0]
+    z = z.reshape(1,z.size)[0]
+    spacex = np.array([])
+    spacey = np.array([])
+    spacez = np.array([])
+    print(' :: Shaping Space :: ')
+    for i in tqdm(range(x.size), desc ='Finding Mandelbulb Set Members'):
+        if member(x[i],y[i],z[i], n=n, itr=itr):
+           spacex = np.append(spacex,x[i])
+           spacey = np.append(spacey,y[i])
+           spacez = np.append(spacez,z[i]) 
     return(spacex,spacey,spacez)
-
-
