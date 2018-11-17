341a56e29236801053a63c60038a289f6873660f
Joey <joey@169-231-119-129.wireless.ucsb.edu>
2018-11-17 13:55:11 -0800

    Changed structure so mandel has funcitons. Shitty save to .txt for super long arrays so dont need to recalc each time

--- Development/main.py
+++ Development/main.py
@@ -1,24 +1,41 @@
-from mandel import space
-import numpy as np
-import matplotlib.pyplot as plt 
-from mpl_toolkits.mplot3d import Axes3D
+import mandel as man
 import time 
 
-def display(Vmax=10,n=8,itr=32):
-	x,y,z = space(Vmax=Vmax, n=n, itr=itr)
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(x,y,z, c='r', marker='o')
-	ax.set_xlabel('X Label')
-	ax.set_ylabel('Y Label')
-	ax.set_zlabel('Z Label')
-
-	fig.show()
-	t=time.perf_counter()
-	input('Press <Enter> to exit')
-	return(t)
-
 if __name__=='__main__':
-	t0 = time.perf_counter()
-	t=display(Vmax=200,n=8)-t0 # keep Vmax^3 reasonable!
-	print('Time elapsed during operation:', t ,'sec')
+	while True:
+		try:
+			ans = input('Recalc or use saved? (r/s) : ')
+			ans = str(ans)
+			ans = ans.lower()
+
+		except Exception:
+			print('Try again')
+
+		else:
+			if ans == 'r':
+				config=input('List config options, comma separated (Vmax, n, itr): ')
+				print(config)
+				Vmax,n,itr=config.strip('()').split(',')
+				Vmax = int(Vmax)
+				n = int(n)
+				itr = int(itr)
+				t0 = time.perf_counter()
+				x,y,z = man.space(Vmax,n,itr)
+				t = man.display(x,y,z)-t0
+				print('Time elapsed during display operation:', t ,'sec')
+				man.save([x,y,z])
+				break
+
+			elif ans == 's':
+				t0 = time.perf_counter()
+				x = man.get_array('Data/1_VNI.txt')
+				y = man.get_array('Data/2_VNI.txt')
+				z = man.get_array('Data/3_VNI.txt')
+				t = man.display(x,y,z) - t0
+				print('Time elapsed during display operation:', t ,'sec')
+				break
+
+			else:
+				print('Try again')
+else:
+	print('main.py unused')
--- Development/mandel.py
+++ Development/mandel.py
@@ -1,6 +1,9 @@
 import numpy as np
 from numba import jit
 from tqdm import tqdm
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+import time
 
 @jit
 def member(Cx, Cy, Cz, n, itr):
@@ -22,12 +25,19 @@ def member(Cx, Cy, Cz, n, itr):
         z = r**n * np.cos(theta*n) + Cz
         
         if x**2 + y**2 + z**2 > 2: # vector length threshold
-            return False # Return boolean logic. If surpass bound after itr, not set number
-    else:
-        return True # If remains bounded through number itr, then set member.
+            return(False) # Return boolean logic. If surpass bound after itr, do not set number
+        else:
+            return(True) # If remains bounded through number itr, then set member.
 
 @jit
-def space(Vmax = 10, n=8, itr=32):
+def sphere(Cx,Cy,Cz,Vmax):
+    if (Cx**2 + Cy**2 + Cz**2 >= Vmax/2):
+        return(False)
+    else:
+        return(True)
+
+@jit
+def space(Vmax = 10, n = 8, itr = 32):
     '''Generates a set of points in 3D space that are members of the mandelbulb set.'''
     print(' :: Generating Meshgrid :: ')
     x,y,z = np.mgrid[-Vmax:Vmax,-Vmax:Vmax,-Vmax:Vmax]
@@ -40,8 +50,71 @@ def space(Vmax = 10, n=8, itr=32):
     spacez = np.array([])
     print(' :: Shaping Space :: ')
     for i in tqdm(range(x.size), desc ='Finding Mandelbulb Set Members'):
-        if member(x[i],y[i],z[i], n=n, itr=itr):
-           spacex = np.append(spacex,x[i])
-           spacey = np.append(spacey,y[i])
-           spacez = np.append(spacez,z[i]) 
+        if member(x[i],y[i],z[i],n,itr):
+            # if not sphere(x[i],y[i],z[i],Vmax):
+            spacex = np.append(spacex,x[i])
+            spacey = np.append(spacey,y[i])
+            spacez = np.append(spacez,z[i])
     return(spacex,spacey,spacez)
+
+def save(listarrays):
+    '''For large arrays, save to .txt so dont need to recalc. Takes in list of arrays'''
+    while True:
+        try:
+            ans = input("Want to save values? (y/n) : ")
+            ans = str(ans)
+            ans = ans.lower()
+        except Exception:
+            print('Try again')
+        else:
+            if ans == 'y':
+                counter=1
+                for i in listarrays:
+                    record(i,title='{}_VNI'.format(counter))
+                    counter+=1
+                print('Saved {} files'.format(counter))
+                break
+            elif ans == 'n':
+                break
+            else:
+                print('Try again')
+
+def record(array,title='Array'):
+    '''Saves array values into .txt with newline separation.'''
+    filestr = ''''''
+    datafile=open('Data/{}.txt'.format(title),'a')
+    for i in range(array.size):
+        filestr+='{} \n'.format(array[i])
+    datafile.write(filestr)
+    datafile.close()
+
+def display(x,y,z):
+    """Display 3D set of coordinates"""
+    fig = plt.figure()
+    xmax = np.amax(x)
+    ymax = np.amax(y)
+    zmax = np.amax(z)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x,y,z, c='r', marker='o')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    ax.set_xlim(-xmax,xmax)
+    ax.set_ylim(-ymax,ymax)
+    ax.set_zlim(-zmax,zmax)
+    fig.show()
+    t=time.perf_counter()
+    input('Press <Enter> to exit')
+    return(t)
+
+def get_array(filename):
+    '''Remake numpy array from .txt file with saved values'''
+    datafile = open('{}'.format(filename),'r')
+    lines = datafile.readlines() # list of the lines
+    array = np.empty(len(lines)) #initialize array of correct size
+    for i in range(len(lines)):
+        array[i]=lines[i] # make array of lines
+        datafile.close()
+    return(array)
+
+
